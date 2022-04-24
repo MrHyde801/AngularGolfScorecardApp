05badcd76b5b31d8d6617c2be4181b29f29daf26
Ian Hyde <mrhyde801@Ians-MacBook-Pro.local>
2022-04-23 21:13:27 -0600

    made small changeds to course-selction component

--- src/app/components/course-selection/course-selection.component.ts
+++ src/app/components/course-selection/course-selection.component.ts
@@ -24,12 +24,12 @@ export class CourseSelectionComponent implements OnInit {
       console.log(courses)
     })
 
-    this.coursesService.getCourse().subscribe(course => {
+  }
+  getCourse(id: string){
+    this.coursesService.getCourse(id).subscribe(course => {
       this.courseInfo = course;
       console.log(course)
     })
   }
   
-  
-
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -13,16 +13,23 @@ export class CoursesService {
     private http: HttpClient
   ) { }
 
+
   getCourses(): Observable<Courses[]>{
     return this.http.get<Courses[]>(`https://golf-courses-api.herokuapp.com/courses/`).pipe(
       map(response => response['courses'])
     )
   }
 
-  getCourse(): Observable<Courses> {
-    return this.http.get<Courses>(`https://golf-courses-api.herokuapp.com/courses/11819`).pipe(
-  
+  getCourse(id: string): Observable<Courses> {
+    return this.http.get<Courses>(`https://golf-courses-api.herokuapp.com/courses/${id}`).pipe(
     )
   }
 
 }
+
+// getCourse(id: string) {
+//   this.coursesService.getCourse(id).subscribe(course => {
+//     this.courseInfo = course;
+//     console.log(course)
+//   })
+// }
